2e0e6923aa9597ec5a9df386de9dc7f38e8f3639
Farhan <farhanisto@gmail.com>
2020-06-01 01:11:40 +0300

    logic for comments route

--- comments/index.js
+++ comments/index.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { randomBytes } = require('crypo')
+const { randomBytes } = require('crypto')
 
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/comments/:id/comments', ()=> {
+const commentsByPostId = {}
 
+app.get('/posts/:id/comments', (req, res)=> {
+
+    const postId = req.params.id 
+    comments = commentsByPostId[postId] || []
+    res.status(200).send(comments)
 })
 
-app.post('/comments/:id/comments', ()=> {
-    
+app.post('/posts/:id/comments', (req, res)=> {
+    const commentId = randomBytes(4).toString('hex')
+    const {content} = req.body
+    const postId = req.params.id
+    const comments = commentsByPostId[postId] || []
+    console.log(comments, 'comments')
+    comments.push({id: commentId, content})
+    commentsByPostId[postId] = comments
+
+    res.status(201).send(comments)
 })
 
 app.listen(4001, ()=> console.log('listening on port 4001'))
