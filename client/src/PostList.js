d082d901efedc3d87468a8ae2e16deef132566fd
Farhan <farhanisto@gmail.com>
2020-06-05 23:33:59 +0300

    fetched posts

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 export default ()=> {
+    const [posts, setPosts] = useState({})
+
+    const getPosts = async ()=>{
+        res = await axios.get('http://localhost:4000')
+        setPosts(res.data)
+    }
+    useEffect(()=>{
+        getPosts
+    },[])
+
+    const renderPosts =  object.values.map(post => {
+        return(
+            <div className="card" key={post.id} style= {{width: '30%', marginBottom: '20px'}}>
+            <div className="card-body"> 
+              {post.title}
+            </div>    
+            </div>
+        )
+    })
+    
     return (
-        <div/>
+        <div className='d-flex flex-row flex-wrap justify-content-between'>
+            {renderPosts}
+        </div>
     )
 }
