b48838bc87af905716fa0f482ce1f291d03cc00c
Farhan <farhanisto@gmail.com>
2020-06-07 17:33:47 +0300

    emitting events from post service

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { randomBytes } = require('crypto')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app =express()
 app.use(bodyParser.json())
@@ -13,10 +14,17 @@ const port = 4000
 let posts = {}
 
 app.get('/posts', (req, res) => {res.send(posts)})
-app.post('/posts', (req, res)=>{
+app.post('/posts', async (req, res)=>{
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
   posts[id] = {id, title}
+  await axios.post('http://localhost:4005/events',{
+      type: 'postCreatedEvent',
+      data: {
+          id,
+          title
+        }
+  })
   res.status(201).send(posts[id])
 })
 
