037f792ff2544c2d74487b6a45c25336014091f6
Farhan <farhanisto@gmail.com>
2020-06-07 18:01:47 +0300

    listening to  events from comments  service

--- comments/index.js
+++ comments/index.js
@@ -33,8 +33,12 @@ app.post('/posts/:id/comments', async (req, res)=> {
             postId
         }
     })
-
     res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+    console.log(`recieved event of type ${req.body.event.type}`)
+    res.send({})
 })
 
 app.listen(4001, ()=> console.log('listening on port 4001'))
