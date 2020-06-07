3e3ac63824d3357e1acef0ff2a3527aa65d7cfa6
Farhan <farhanisto@gmail.com>
2020-06-07 23:39:19 +0300

    added posts route to fetch all posts and commits

--- query-service/index.js
+++ query-service/index.js
@@ -5,11 +5,23 @@ const cors = require('cors')
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
-const postsNComments = {} // {postId:{postId, postTitle, comments: [{id, comment, postid}]}}
+const postsNComments = {} // {postId:{postId, postTitle, comments: [{id, comment, postids}]}}
 app.post('/events',(req, res)=> {
-  const eventType = req.body.event.type 
+  const {type, data} = req.body.event
   console.log(eventType, 'eventType')
+  if(type === 'postCreatedEvent'){
+      const {id, title} = data
+      postsNComments[id] = {id, title, comments: []}
+  }
+  if(type === 'commentCreatedEvent'){
+      const {id, content, postId} = data
+      postsNComments[postId].comments.push({id,content})
+  }
   res.send({status:201})
+})
+
+app.get('/posts', (req, res)=>{
+    res.status({"status": 200}).send(postsNComments)
 })
 
 app.listen(4002, ()=> {
