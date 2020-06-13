5c8e9fe85f6bfd786b48fa2cd14689e4d09f828e
Farhan <farhanisto@gmail.com>
2020-06-13 20:08:18 +0300

    emitting events from  our moderation service

--- moderation-service/index.js
+++ moderation-service/index.js
@@ -5,7 +5,22 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/events', (req, res)=>{
+app.post('/events', async (req, res)=>{
+    const {type, data} = req.body.event 
+
+    if (type === 'commentCreatedEvent'){
+        status = data.content.includes('orange')? 'rejected': 'approved'
+        await axios.post('http://localhost:4005/events', {
+            type: 'commentModeratedEvent',
+            data: {
+                content: data.content,
+                status,
+                postId: data.postId,
+                id: data.id
+            }
+        })
+    }
+    res.send({})
 
 })
 
