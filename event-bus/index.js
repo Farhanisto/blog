5a58cb1bbad44fa289d56db969766e26c37c4680
Farhan <farhanisto@gmail.com>
2020-06-13 19:56:46 +0300

    emitting events to our moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,6 +16,9 @@ app.post('/events',async (req, res)=>{
     await axios.post('http://localhost:4002/events', {
         event
     }).catch (e  => {console.log(e)})
+    await axios.post('http://localhost:4003/events', {
+        event
+    })
     res.send({status: '0k'})
 })
 
