98726c931af6d445893804af8a52f0d631882c27
Farhan <farhanisto@gmail.com>
2020-06-13 19:05:32 +0300

    moderation service setup -bug fixes

--- moderation-service/index.js
+++ moderation-service/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const axios = require(axios)
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/events', (req, res){
-    
+app.post('/events', (req, res)=>{
+
 })
 
 app.listen(4003, ()=>console.log('app listening on port 4003'))
