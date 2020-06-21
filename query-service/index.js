f00473326bed56b0d3e4e082a15b68a0ab2e0d5a
Farhan <farhanisto@gmail.com>
2020-06-21 21:21:59 +0300

    fetching all events in query service in case it goes down

--- query-service/index.js
+++ query-service/index.js
@@ -1,29 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 const postsNComments = {} // {postId:{postId, postTitle, comments: [{id, comment, postids}]}}
+const handleEvents = (type, data)=> {
+    if(type === 'postCreatedEvent'){
+        const {id, title} = data
+        postsNComments[id] = {id, title, comments: []}
+    }
+    if(type === 'commentCreatedEvent'){
+        const {id, content, postId, status} = data
+        postsNComments[postId].comments.push({id, content, status})
+    }
+    if(type === 'commentUpdatedEvent'){
+        const {id,status,postId,content} = data
+        console.log(postsNComments, 'postsncome')
+        const commentToUpdate = postsNComments[postId].comments.find(comment => comment.id === id)
+        commentToUpdate.status = status
+        commentToUpdate.content = content
+    }
+}
 app.post('/events',(req, res)=> {
   const {type, data} = req.body.event
-  console.log(type, 'eventType')
-  if(type === 'postCreatedEvent'){
-      const {id, title} = data
-      postsNComments[id] = {id, title, comments: []}
-  }
-  if(type === 'commentCreatedEvent'){
-      const {id, content, postId, status} = data
-      postsNComments[postId].comments.push({id, content, status})
-  }
-  if(type === 'commentUpdatedEvent'){
-      const {id,status,postId,content} = data
-      console.log(postsNComments, 'postsncome')
-      const commentToUpdate = postsNComments[postId].comments.find(comment => comment.id === id)
-      commentToUpdate.status = status
-      commentToUpdate.content = content
-  }
+  handleEvents(type, data)
   res.send({status:201})
 })
 
@@ -32,6 +35,11 @@ app.get('/posts', (req, res)=>{
     res.status(200).send(postsNComments)
 })
 
-app.listen(4002, ()=> {
+app.listen(4002, async()=> {
     console.log('listening on port 4002')
+    const res = await axios.get('http:localhost:4005/events')
+    for(event in res.data){
+        const {type, data} = event
+        handleEvents(type, data)
+    }
 })
